2cfea338d2239f1ec544ebd5dc131ccf00448017
Andriy Lukashchuk <Andriy.Lukashchuk@theaccessgroup.com>
2023-01-31 18:04:45 +0200

    refactor

--- lib/ga4-rails/model.rb
+++ lib/ga4-rails/model.rb
@@ -17,6 +17,16 @@ module Ga4Rails::Model
     def results(access_token:, property_id:, start_date:, end_date:, limit: 1000)
       client = Ga4Rails::Client.new(access_token: access_token)
 
+      # TODO: Handle response
+      client.run_property_report(
+        property_id: property_id,
+        body: build_body(start_date: start_date, end_date: end_date, limit: limit)
+      ).response
+    end
+
+    private
+
+    def build_body(start_date:, end_date:, limit:)
       body = {}
       body[:date_ranges] = [
         {
@@ -27,11 +37,8 @@ module Ga4Rails::Model
       body[:metrics] = @metrics.map { |metric| { name: metric } }
       body[:dimensions] = @dimensions.map { |dimension| { name: dimension } }
       body[:limit] = limit
-
-      client.run_property_report(
-        property_id: property_id,
-        body: body
-      ).response
+      
+      body
     end
   end
 end
